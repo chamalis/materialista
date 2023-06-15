7c166b430290e984bb20b47d45d2a44e269b570f
Your Name <you@example.com>
2023-06-15 20:12:12 +0200

    fix typo

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ MAT_DIR="${VENV_DIR}/materialista"
 
 mkdir -p "$VENV_DIR"
 virtualenv -p python3 "$MAT_DIR"
-source "{MAT_DIR}"/bin/activate
+source "${MAT_DIR}/bin/activate"
 pip install -r requirements.txt
